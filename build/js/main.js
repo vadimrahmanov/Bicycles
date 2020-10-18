58c3db8790c64e4b4d25c8e5b7e4908d44a13247
Vadim Rahmanov <VadimRahmanov@gmail.com>
2020-10-18 16:14:36 +0100

    Финальные правки v3

--- build/js/main.js
+++ build/js/main.js
@@ -1,16 +1,23 @@
 'use strict';
 
 (function () {
+  var header = document.querySelector('header');
   var navigation = document.querySelector('.navigation');
-  var navigationHandler = document.querySelector('.navigation button');
-  var navigationList = navigation.querySelector('ul');
 
-  navigationList.classList.add('nav-close');
+  if (header.contains(navigation)) {
+    var navigationHandler = document.querySelector('.navigation button');
+    var navigationList = navigation.querySelector('ul');
 
-  navigationHandler.addEventListener('click', function () {
-    navigationList.classList.toggle('nav-close');
-    navigationList.classList.toggle('nav-opened');
-    navigation.classList.toggle('navigation--opened');
-    navigationHandler.classList.toggle('nav-btn-opened');
-  });
+    navigationList.classList.add('nav-close');
+
+    navigationHandler.addEventListener('click', function () {
+      navigationList.classList.toggle('nav-close');
+      navigationList.classList.toggle('nav-opened');
+      navigation.classList.toggle('navigation--opened');
+      navigationHandler.classList.toggle('nav-btn-opened');
+    });
+  }
+
+  // eslint-disable-next-line no-undef
+  svg4everybody();
 })();
