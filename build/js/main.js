235d5bbb1c29f5081952c5e2355df12924bab498
Vadim Rahmanov <VadimRahmanov@gmail.com>
2020-11-24 17:44:05 +0000

    Final Project v2

--- build/js/main.js
+++ build/js/main.js
@@ -4,6 +4,7 @@
   var header = document.querySelector('header');
   var navigation = document.querySelector('.navigation');
   var body = document.querySelector('body');
+  var menuItem = navigation.querySelectorAll('a');
 
   if (header.contains(navigation)) {
     var navigationHandler = document.querySelector('.navigation button');
@@ -18,6 +19,14 @@
       navigationHandler.classList.toggle('nav-btn-opened');
       body.classList.toggle('nav-opened');
     });
+
+    for (var i = 0; i < menuItem.length; i++) {
+      menuItem[i].addEventListener('click', function () {
+        body.classList.remove('nav-opened');
+        navigationList.classList.add('nav-close');
+        navigationHandler.classList.remove('nav-btn-opened');
+      });
+    }
   }
 
   // eslint-disable-next-line no-undef
